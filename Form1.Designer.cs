8abf33ae0c0e077d50e493f310653d7f6ecbcb3c
Nicollas Isaac <79726241+NicollasIsaac@users.noreply.github.com>
2021-09-20 02:40:29 -0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace CampoMinado
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(286, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(289, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(475, 331);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "INICIAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CampoMinado.Properties.Resources.download;
+            this.pictureBox1.Location = new System.Drawing.Point(56, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(70, 61);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CampoMinado
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Parent = pictureBox1;
+        }
+
+        int[] vetorNum = new int[49];
+        int pontuaçãoTotal = 0;
+        int numAleatorio = 0;
+        Random rdne = new Random();
+
+        void GerarVetor()
+        {
+            int conte = 0;
+            for (int i = 0; i < vetorNum.Length; i++)
+                vetorNum[i] = 0;
+
+            for(int i = 0; i<49; i++)
+            {
+                do
+                {
+                    numAleatorio = rdne.Next(5, 10);
+                    conte = 0;
+                    foreach (int numero in vetorNum)
+                    {
+                        if (numAleatorio == numero)
+                            conte++;
+                    }
+                } while (conte == 49);
+                vetorNum[i] = numAleatorio;
+            }
+        }
+
+      
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        PictureBox[] btn = new PictureBox[49];
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GerarVetor();
+
+            int x = 0, y = 0;
+
+            for(int i = 0; i > 49; i++)
+            {
+                if (x == 400)
+                {
+                    x = 0;
+                    y += 100;
+                }
+                btn[i] = new PictureBox();
+                btn[i].Parent = pictureBox1;
+                btn[i].Left += 200 + x;
+                btn[i].Top += 50 + y;
+                btn[i].Width = 80;
+                btn[i].Height = 80;
+                btn[i].Load("download.jpg");
+                btn[i].Click += new EventHandler(btnClick);
+                x += 100;
+            }
+          
+        }
+
+        void sorteador ()
+        {        
+            Random numero = new Random();
+            int sorte = numero.Next(5, 10);
+            pontuaçãoTotal += sorte;
+        }
+
+        void btnClick(object sender, EventArgs e)
+        {
+            sorteador();
+            if(pontuaçãoTotal > 25)
+            {
+                MessageBox.Show("Você expludiu! Tente novamente.");
+                Application.Restart();
+            }
+
+            label1.Text = pontuaçãoTotal.ToString();
+        }
+    }
+}
